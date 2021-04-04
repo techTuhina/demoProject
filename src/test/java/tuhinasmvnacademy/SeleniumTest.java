0bfca768f5e8e5905f3a4020e5476aaccbaf1f53
Tuhina <tuhinabasu.ind@gmail.com>
2021-04-04 20:32:19 +0530

    first commit

---
+++ src/test/java/tuhinasmvnacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package tuhinasmvnacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+@Test
+public void Webautomation() {
+	System.out.println("Webautomation");
+	}
+@Test	
+	public void WebElementUI() {
+		System.out.println("WebElementUI");
+	}
+}
---
+++ src/test/java/tuhinasmvnacademy/SimpleJavaTest.java
@@ -0,0 +1,20 @@
+package tuhinasmvnacademy;
+
+import org.testng.annotations.Test;
+
+public class SimpleJavaTest {
+	
+@Test
+	public void Simpleautomation() {
+		
+		System.out.println("Simpleautomation");
+		
+	}
+@Test
+	public void ElementUI() {
+		
+		System.out.println("ElementUI");
+		
+	}
+
+}
